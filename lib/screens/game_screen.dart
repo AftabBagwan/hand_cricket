eb24b94d55102b3d0398ca0bd10de80a4a92cc3c
Aftab Bagwan <aftabbagwan9886@gmail.com>
2025-04-24 10:38:47 +0530

    feat: run button on game screen added

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,20 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:hand_cricket/utils/assets.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: Stack(
         children: [
           Positioned.fill(
-            child: Image.asset(
-              "assets/background.png",
-              fit: BoxFit.cover,
+            child: Image.asset("assets/background.png", fit: BoxFit.cover),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: MediaQuery.sizeOf(context).height * 0.1,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    runButton(
+                      context,
+                      image: Assets.oneImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                    runButton(
+                      context,
+                      image: Assets.twoImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                    runButton(
+                      context,
+                      image: Assets.threeImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    runButton(
+                      context,
+                      image: Assets.fourImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                    runButton(
+                      context,
+                      image: Assets.fiveImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                    runButton(
+                      context,
+                      image: Assets.sixImage,
+                      onTap: () {},
+                      screenSize: size,
+                    ),
+                  ],
+                ),
+              ],
             ),
-          )
+          ),
         ],
+      ),
+    );
+  }
+
+  GestureDetector runButton(
+    BuildContext context, {
+    required Size screenSize,
+    required String image,
+    required VoidCallback onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset(
+        image,
+        height: screenSize.height * 0.1,
+        width: screenSize.width * 0.2,
       ),
     );
   }
---
+++ lib/utils/assets.dart
@@ -0,0 +1,8 @@
+class Assets {
+  static const String oneImage = "assets/one.png";
+  static const String twoImage = "assets/two.png";
+  static const String threeImage = "assets/three.png";
+  static const String fourImage = "assets/four.png";
+  static const String fiveImage = "assets/five.png";
+  static const String sixImage = "assets/six.png";
+}

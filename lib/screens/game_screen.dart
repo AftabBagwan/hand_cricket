c46bb4ae13b6867bc638a26e1d0abe8fe18aafd1
Aftab Bagwan <aftabbagwan9886@gmail.com>
2025-04-24 10:21:43 +0530

    feat: added required configuration in pub file and initial game screen added

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/background.png",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
